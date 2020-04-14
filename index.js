77898f4c3d93bff5e02363dd21d35556c63a1456
Kaseaa <>
2020-04-15 01:18:14 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 module.exports = function KaseaIsMyDaddy(mod) {
-    let blocked_abnormies = [];
-    mod.game.data.abnormalities.forEach(abnormality=> {
-        if(abnormality.effects.some(effect => effect.type === 244)) {
-            blocked_abnormies.push(abnormality.id);
-        }
-    });
-
-    const blocker = e=> blocked_abnormies.includes(e.id) ? false : undefined;
+    function blocker(event) {
+        const abnormality = mod.game.data.abnormalities.get(event.id);
+        if (abnormality && abnormality.effects.some(effect => effect.type === 244))
+            return false;
+    }
     mod.hook("S_ABNORMALITY_BEGIN", 3, {order: 10000, filter: {fake: null}}, blocker);
     mod.hook("S_ABNORMALITY_REFRESH", 1, {order: 10000, filter: {fake: null}}, blocker);
 }
